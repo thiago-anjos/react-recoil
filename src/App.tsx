3eea672f3029ff7af39a1fb8b356e22be6d15687
thiago <thiago.w3c@gmail.com>
2025-11-30 18:37:02 -0300

    move events to recoil from form part 1

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
       <div className={style.App}>
         <div className={style.Coluna}>
           <Card>
-            <Formulario aoSalvar={adicionarEvento} />
+            <Formulario />
           </Card>
           <hr />
           <Card>
